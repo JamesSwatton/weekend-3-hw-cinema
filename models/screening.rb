9fc3a5d111bc73e93dcd8f181a47ee42a7b79f2d
James Swatton <jamessswatton@gmail.com>
2019-04-21 16:48:43 +0100

    add capacity/available_seats instance variables to Screening class. add capacity/available_seats columns to screenings table

--- models/screening.rb
+++ models/screening.rb
@@ -7,11 +7,13 @@ class Screening
     @id = options['id'].to_i if options['id']
     @film_id = options['film_id'].to_i
     @show_time = options['show_time']
+    @capacity = options['capacity'].to_i
+    @available_seats = ['available_seats'].to_i
   end
 
   def save()
-    sql = "INSERT INTO screenings (film_id, show_time) VALUES ($1,$2) RETURNING id;"
-    values = [@film_id, @show_time]
+    sql = "INSERT INTO screenings (film_id, show_time, capacity, available_seats) VALUES ($1,$2,$3,$4) RETURNING id;"
+    values = [@film_id, @show_time, @capacity, @available_seats]
     screenings = SqlRunner.run(sql, values)
     @id = screenings.first['id'].to_i
   end
